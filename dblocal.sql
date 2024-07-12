a2a76c14ae156dd0c75d1ffa9dce4248912cc842
Nancy <profenancygutierrez@gmail.com>
2024-07-12 20:43:56 -0300

    agrego target

---
+++ dblocal.sql
@@ -0,0 +1,64 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 11-07-2024 a las 19:03:55
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `dblocal`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `seriesypeliculas`
+--
+
+CREATE TABLE `seriesypeliculas` (
+  `id` int(11) NOT NULL,
+  `titulo` varchar(255) NOT NULL DEFAULT 'NOT NULL',
+  `clasificacion` varchar(150) NOT NULL DEFAULT 'NOT NULL',
+  `año` int(150) NOT NULL,
+  `director` varchar(150) NOT NULL DEFAULT 'NOT NULL',
+  `descripcion` varchar(250) NOT NULL DEFAULT 'NOT NULL',
+  `generos` varchar(250) NOT NULL DEFAULT 'NOT NULL',
+  `url` varchar(255) NOT NULL DEFAULT 'NOT NULL'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `seriesypeliculas`
+--
+ALTER TABLE `seriesypeliculas`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `seriesypeliculas`
+--
+ALTER TABLE `seriesypeliculas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
